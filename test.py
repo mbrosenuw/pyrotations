74697c7c08975e6f1946fd6337fa44692d63f2e8
mbrosenuw <mbrosen@uw.edu>
2025-06-11 14:06:16 -0700

    compatibility fixes

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-from pyrotations import pyrotations
+import pyrotations
 import numpy as np
 from scipy.sparse import issparse
 import pandas as pd
 
 # all the constants you would want to use
 mu = [0, 1, 0]  # b axis
-jmin = 18
-jmax = 19
+jmin = 0
+jmax = 30
 T = 77
 lims = [-40, 40]
 shift = 0
@@ -47,16 +47,16 @@ def sparse_to_csv(matrix, basis_labels, filename, threshold=1e-10):
     df.to_csv(filename)
 
 
-label_tuples = lambda basis: [str(t) for t in basis]
-sparse_to_csv(ja, label_tuples(rotbasis), "Ja.csv")
-sparse_to_csv(jb, label_tuples(rotbasis), "Jb.csv")
-sparse_to_csv(jc, label_tuples(rotbasis), "Jc.csv")
-sparse_to_csv(Hrot, label_tuples(rotbasis), "Hrot.csv")
-sparse_to_csv(dipole, label_tuples(rotbasis), "dipole.csv")
-
-# or as a numpy file
-np.savez('operators', ja=ja.toarray(), jb=jb.toarray(), jc=jc.toarray(),
-         Hrot=Hrot.toarray(), dipole=dipole.toarray())
+# label_tuples = lambda basis: [str(t) for t in basis]
+# sparse_to_csv(ja, label_tuples(rotbasis), "Ja.csv")
+# sparse_to_csv(jb, label_tuples(rotbasis), "Jb.csv")
+# sparse_to_csv(jc, label_tuples(rotbasis), "Jc.csv")
+# sparse_to_csv(Hrot, label_tuples(rotbasis), "Hrot.csv")
+# sparse_to_csv(dipole, label_tuples(rotbasis), "dipole.csv")
+#
+# # or as a numpy file
+# np.savez('operators', ja=ja.toarray(), jb=jb.toarray(), jc=jc.toarray(),
+#          Hrot=Hrot.toarray(), dipole=dipole.toarray())
 
 # calculate the spectrum, and save to a file !
 DMS.newcalcspectrum(save=True, name='DMS_J18-19')
